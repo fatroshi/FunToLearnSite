a718cb6ebec6e16c12260a786ae3eea825032c95
Farhad <fa@neodesign.se>
2015-12-19 15:02:42 +0100

    DB classes

--- app.php
+++ app.php
@@ -1,11 +1,23 @@
 <?php include_once("includes/DB/Controller.php") ?>
 
+
+<gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01" xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+    <gesmes:subject>Reference rates</gesmes:subject>
+    <gesmes:Sender>
+        <gesmes:name>European Central Bank</gesmes:name>
+    </gesmes:Sender>
+    <Cube
+
 <?php
     $controller = new Controller();
 
     $items = $controller->getAllItems();
 
     //$controller->var_dump($items);
+    ?>
+
+    <Cube time="2015-11-06">
+    <?php
 
     foreach($items as $item){
         // array($row['id'],$row['categoryName'],$item['imgName'],$item['itemName']);
@@ -14,12 +26,18 @@
         $imgName                =  $item[2];
         $itemName               =  $item[3];
 
+        //<Cube currency="USD" rate="1.0864"/>
+
         echo "Category Id: " . $categoryId . "<BR>";
         echo "Category name: " . $categoryName  . "<BR>";
         echo "Item name: " . $itemName  . "<BR>";
         echo "Image: " . $imgName    . "<BR>";
-        echo "<hr>";
+        echo "<Cube catId='$categoryId' catName='$categoryName' imgName='$imgName' itemName='$itemName'>";
 
     }
 
-?>
+    ?>
+        </Cube>
+
+    </Cube>
+</gesmes:Envelope>
